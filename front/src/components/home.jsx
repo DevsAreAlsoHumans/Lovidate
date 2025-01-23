027acc14f6b6e83a8883d1c9a2703deecece1efe
Theo Mousnier <mousniert.pro@gmail.com>
2025-01-23 16:10:40 +0100

    add inscription

--- front/src/components/home.jsx
+++ front/src/components/home.jsx
@@ -3,24 +3,27 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Profil from './profil';
 import Notification from './notification';
 import Messagerie from './message';
+import Inscription from "./inscription.jsx";
 
 const Home = () => {
   return (
     <Router>
       <div>
         <h1>Bienvenue sur l'application</h1>
-        <nav>
+        <footer>
           <ul>
             <li><Link to="/profil">Profil</Link></li>
             <li><Link to="/notification">Notification</Link></li>
             <li><Link to="/message">Tchat</Link></li>
+            <li><Link to="/inscription">Inscription</Link></li>
           </ul>
-        </nav>
+        </footer>
 
         <Routes>
           <Route path="/profil" element={<Profil />} />
           <Route path="/notification" element={<Notification />} />
           <Route path="/message" element={<Messagerie />} />
+          <Route path="/inscription" element={<Inscription />} />
           <Route path="/" element={<h2>Bienvenue sur la page d'accueil</h2>} />
         </Routes>
       </div>
