45bb560754c244ccfe8ac49e5c04448f99c581b0
Kim <isabelle.kim2023@campus-eni.fr>
2025-01-24 09:17:26 +0100

    front profil done

--- front/src/components/home.jsx
+++ front/src/components/home.jsx
@@ -3,25 +3,38 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Profil from './profil';
 import Notification from './notification';
 import Messagerie from './message';
+import { FaHome, FaComments } from 'react-icons/fa';
+//import { Link, Routes, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
     <Router>
       <div>
-        
+        <header>
+          <h1 className='title-home'>Lovidate</h1>
+        </header>
+
         <Routes>
           <Route path="/profil" element={<Profil />} />
           <Route path="/notification" element={<Notification />} />
           <Route path="/message" element={<Messagerie />} />
           <Route path="/" element={<h2>Bienvenue sur la page d'accueil</h2>} />
         </Routes>
-        <footer>
-          <ul>
-            <li><Link to="/message">Tchat</Link></li>
-          </ul>
-        </footer>
 
-       
+        <nav>
+          <ul>
+            <li>
+              <Link to="/profil">
+                <FaHome size={30} title="Accueil" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/message">
+                <FaComments size={30} title="Tchat" />
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </Router>
   );
